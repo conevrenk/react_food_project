910cbeb814c32458df8db7a8994c07256f832712
conevrenk <155632852+conevrenk@users.noreply.github.com>
2024-10-21 09:31:15 +0300

    events addes

---
+++ src/data.js
@@ -0,0 +1,30 @@
+ export const pizzas = [
+    {
+      title: "Karışık Pizza",
+      description: "Seper harika pizza",
+      image: "pizza1.jpg",
+      price: 400,
+      is_active:true
+    },
+    {
+      title: "peynirli  Pizza",
+      description: "Seper harika pizza",
+      image: "pizza2.jpg",
+      price: 200,
+      is_active:true
+    },
+    {
+      title: "Sucuklu Pizza",
+      description: "Seper harika pizza",
+      image: "pizza3.jpg",
+      price: 500,
+      is_active:true
+    },
+    {
+      title: "Acılı  Pizza",
+      description: "Seper harika pizza",
+      image: "pizza4.jpg",
+      price: 640,
+      is_active:true
+    },
+  ];
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
+import Events from "./components/Events";
 
-
-
-
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+    // <App />
+    <Events/>
+);
